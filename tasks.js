7f7ee56d7b311ee460f6890ef0fd66b54478d857
DanaOnopchenko <106885955+DanaOnopchenko@users.noreply.github.com>
2023-12-23 03:45:54 +0200

    update tasks

--- tasks.js
+++ tasks.js
@@ -156,28 +156,28 @@
 //     console.log('I am sorry')
 // }
 
-function checkStorage(available, ordered) {
-  let message;
+
+function checkForSpam(message) {
+  let result;
   // Change code below this line
-
-  message = ordered > available ? "Not enough goods in stock!" : "The order is accepted, our manager will contact you"
-  // if (ordered > available) {
-  //   message = "Not enough goods in stock!";
-  // } else {
-  //   message = "The order is accepted, our manager will contact you";
-  // }
-
+ 
+result= message.toLocaleLowerCase().includes("spam") || message.toLocaleLowerCase().includes("sale")
   // Change code above this line
-  return message;
+  return result;
 }
 
-console.log(checkStorage(100, 130));
-// Виконай рефакторинг рішення задачі «Склад товарів», замінивши інструкцію if...else тернарним оператором.
+console.log(checkForSpam("Amazing SalE, only tonight!"));
+// Функція checkForSpam(message) приймає рядок (параметр message), перевіряє його на вміст заборонених слів spam і sale, і повертає результат перевірки. Слова в рядку параметра message можуть бути у довільному регістрі, наприклад SPAM або sAlE.
 
-// Оголошена функція checkStorage(available, ordered).
-// Використаний тернарний оператор.
-// Виклик checkStorage(100, 50) повертає "The order is accepted, our manager will contact you"
-// Виклик checkStorage(100, 130) повертає "Not enough goods in stock!"
-// Виклик checkStorage(200, 20) повертає "The order is accepted, our manager will contact you"
-// Виклик checkStorage(200, 150) повертає "The order is accepted, our manager will contact you"
-// Виклик checkStorage(150, 180) повертає "Not enough goods in stock!"
+// Якщо знайшли заборонене слово (spam або sale) то функція повертає буль true.
+// Якщо в рядку відсутні заборонені слова, функція повертає буль false.
+
+// Оголошена функція checkForSpam(message).
+// Виклик функції checkForSpam("Latest technology news") повертає false
+// Виклик функції checkForSpam("JavaScript weekly newsletter")повертає false
+// Виклик функції checkForSpam("Get best sale offers now!") повертає true
+// Виклик функції checkForSpam("Amazing SalE, only tonight!") повертає true
+// Виклик функції checkForSpam("Trust me, this is not a spam message") повертає true
+// Виклик функції checkForSpam("Get rid of sPaM emails. Our book in on sale!") повертає true
+// Виклик функції checkForSpam("[SPAM] How to earn fast money?") повертає true
+
