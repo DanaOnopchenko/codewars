95707cb67a4af4ded1297109e111975c34bc3db8
DanaOnopchenko <106885955+DanaOnopchenko@users.noreply.github.com>
2024-01-05 01:23:26 +0200

    update tasks

--- js/massive.js
+++ js/massive.js
@@ -105,13 +105,163 @@ const cart = [54, 28, 105, 70, 92, 17, 120,12,96];
  * при условии что числа уникальные (не повторяются).
  */
 
-const numbers = [51, 18, 13, 24, 7, 85, 19];
-let smaliesNumber = numbers[0];
-for (const number of numbers) { 
+// const numbers = [51, 18, 13, 24, 7, 85, 19];
+// let smaliesNumber = numbers[0];
+// for (const number of numbers) {
     
-    if (number < smaliesNumber) { 
-        smaliesNumber = number;
-    }
-}
+//     if (number < smaliesNumber) {
+//         smaliesNumber = number;
+//     }
+// }
 
-console.log(smaliesNumber);
+// console.log(smaliesNumber);
+
+
+// const numbers = [51, 18, 13, 24, 7, 85, 19];
+// let biggestNumber = numbers[0];
+// for (const number of numbers) {
+//     if (number > biggestNumber) {
+//         biggestNumber = number;
+//     }
+// }
+// console.log(biggestNumber);
+
+
+// Task 6
+// Напиши скрипт, который объединяет все элементы массива в одно строковое значение.
+//   Элементов может быть произвольное кол-во.
+//   Пусть элементы массива  в строке будут разделены запятой.
+
+// const friends = ['Mango', 'Poly', 'Kiwi', 'Ajax'];
+// // let string = '';
+// // for (const friend of friends) {
+// //     string += friend + ', ';
+// // }
+// // string = string.slice(0, string.length - 2);
+// // console.log(string);
+// const string = friends.join(', ');
+// console.log(string);
+
+
+// Task 7
+
+/*
+ * Напиши скрипт который заменяет регистр каждого символа в строке на противоположный.
+ * Например, если строка «JavaScript», то на выходе должена быть строка «jAVAsCRIPT».
+ */
+
+// const string = 'JavaScript';
+// const letters = string.split('');
+
+// let invertedStr = '';
+// for (const letter of letters) {
+//     if (letter === letter.toLowerCase()) {
+        
+//         invertedStr += letter.toUpperCase();
+//     } else {
+//         invertedStr += letter.toLowerCase();
+//     }
+//     console.log(letter);
+// }
+
+// for (const letter of letters) {
+//     invertedStr +=
+//         letter === letter.toLowerCase() ? letter.toUpperCase() : letter.toLowerCase()
+// }
+
+// console.log(invertedStr);
+
+
+// Task 8
+
+/*
+ * Делаем slug в URL из названия статьи (например на dev.to)
+ * Заголовок статьи состоит только из букв и пробелов
+ *
+ * - Нормализируем строку
+ * - Разбиваем по словам
+ * - Сшиваем в строку с разделителями
+ */
+
+// Должно получиться top-10-benefits-of-react-framework
+// const title = 'Top 10 benefits of React framework';
+
+// const normTitle = title.toLowerCase().split(' ');
+// const slugStr = normTitle.join('-');
+
+// const slug = title.toLowerCase().split(' ').join('-')
+// console.log(slugStr);
+// console.log(slug);
+
+// Task 9
+/*
+ * Напиши скрипт который считает сумму элементов двух массивов.
+ */
+
+// const array1 = [5, 10, 15, 20];
+// const array2 = [10, 20, 30];
+
+// const numbers = array1.concat(array2);
+// let total = 0;
+
+// // for (let i = 0; i < numbers.length; i += 1) {
+// //     console.log(numbers[i]);
+// //     total += numbers[i];
+// // }
+
+// for (const number of numbers) {
+//     total += number;
+// }
+
+// console.log(total);
+
+// Task 10
+/*
+ * Работем с коллекцией карточек в trello
+ * - Метод splice()
+ * - Удалить
+ * - Добавить
+ * - Обновить
+ */
+
+const cards = [
+  'Карточка-1',
+  'Карточка-2',
+  'Карточка-3',
+  'Карточка-4',
+  'Карточка-5',
+];
+// const cardToRemove = 'Карточка-3';
+// const index = cards.indexOf(cardToRemove);
+
+
+// console.table(cards)
+// console.log(index);
+// cards.splice(index, 3);
+
+// console.table(cards);
+
+/*
+ * Добавление (по индексу)
+ */
+
+// const cardToInsert = 'Карточка-6';
+// const index = 5;
+// cards.splice(index,0, cardToInsert);
+// console.table(cards);
+
+/*
+ * Обновление (по индексу)
+ */
+
+
+const cardToUpdate = 'Карточка-4';
+
+
+// const index = cards.indexOf(cardToUpdate);
+
+const index = cards.indexOf(cardToUpdate);
+console.log(index)
+cards.splice(index,1,"updatecard")
+
+console.table(cards);
