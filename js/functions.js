edafd127671ef8338d7128883497ca2060dfb4dc
DanaOnopchenko <106885955+DanaOnopchenko@users.noreply.github.com>
2024-01-09 01:12:16 +0200

    update

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,4 @@
+// Task 1
 // const add = function add(a, b) {
 //     console.log(a);
 //     console.log(b);
@@ -17,16 +18,75 @@
 // }
 // console.log(fn());
 
+
+// Task 2
 /*
  * Напиши функцию calculateTotalPrice(items)
  * которая принимает массив цен (чисел) и возвращает их сумму
  */
 
-const cart = [54, 28, 105, 70, 92, 17, 120, 12, 25, 90];
-let total = 0;
+// const cart = [54, 28, 105, 70, 92, 17, 120, 12, 25, 90];
+// let total = 0;
 
-for (const value of cart) {
-  total += value;
+// for (const value of cart) {
+//   total += value;
+// }
+
+// console.log('Total: ', total);
+
+// const calculateTotalPrice = function (items) {
+//   console.log(items);
+//   let total = 0;
+//   for (const item of items) {
+//     total += item;
+//   }
+//   return total;
+
+// }
+
+// console.log(calculateTotalPrice([1, 2, 3]));
+// console.log(calculateTotalPrice([5, 10, 15, 20])); // 50
+// console.log(calculateTotalPrice([100, 200, 300])); // 600
+
+// Task 3
+/*
+ * Напиши функцию logItems(items) для перебора и логирования массива
+ */
+
+
+// const logItems = function (items) {
+//   for (const item of items) {
+//     console.log(item);
+//   }
+// }
+
+// logItems(['Mango', 'Kiwi', 'Poly', 'Ajax']);
+// logItems([1, 2, 3, 4, 5]);
+// logItems(['клавиатура', 'наушники', 'часы']);
+
+// Task 4
+
+/*
+ * Напиши функцию findLogin(allLogins, login) для поиска логина
+ * - Если логина нет, вывести сообщение 'Пользователь [логин] не найден.'
+ * - Если нашли логин, вывести сообщение 'Пользователь [логин] найден.'
+ */
+
+const logins = ['m4ngoDoge', 'k1widab3st', 'poly1scute', 'aj4xth3m4n'];
+
+
+const findLogin = function (allLogins, loginToFind) { 
+  
+   for (const login of allLogins) { 
+    if (login === loginToFind) { 
+     return `Пользователь ${loginToFind} найден.`;
+    }
+     }
+  return `Пользователь ${loginToFind} не найден.`
 }
 
-console.log('Total: ', total);
+
+console.log(findLogin(logins, 'avocod3r'));
+console.log(findLogin(logins, 'k1widab3st'));
+console.log(findLogin(logins, 'jam4l'));
+console.log(findLogin(logins, 'poly1scute'));
