1125357651f6476670a36234ff5c537c521916f1
DanaOnopchenko <106885955+DanaOnopchenko@users.noreply.github.com>
2024-01-15 01:54:32 +0200

    update

--- js/functions.js
+++ js/functions.js
@@ -203,8 +203,8 @@
 // const filterNumbers = function (array, ...args) {
     
 //     let uniqueElements = [];
-//     for (const element of array) { 
-//         if (args.includes(element)) { 
+//     for (const element of array) {
+//         if (args.includes(element)) {
 //             uniqueElements.push(element);
 //         }
 //     }
@@ -215,3 +215,113 @@
 // console.log(filterNumbers([1, 2, 3, 4, 5], 10, 15, 2, 3, 8)); // [2, 3]
 // console.log(filterNumbers([10, 15, 25, 30], 23, 30, 18, 15)); // [30, 15]
 // console.log(filterNumbers([100, 200, 300, 400, 500], 7, 12, 200, 64)); // [200]
+
+
+
+// =====================================================
+/// Створення та види функцій
+
+// function expression
+// викликається тільки після оголошення
+// const world = 'world';
+// const hello = 'Hello';
+// // foo(world, hello); буде помилка!!!
+
+
+// const foo = function (hello, world) {
+//     console.log(hello, world);
+// };
+// foo(world, hello);
+
+
+
+
+// function declaretion
+
+// boo(hello, world);
+
+// function boo(hello, world) {
+//     console.log(hello, world);
+// }
+
+
+
+// Task 11
+
+// arguments
+//  Порахувати суму всіх чисел
+
+// function getSum() {
+//     console.log(arguments);
+//     let total = 0;
+//     // for (const arg of arguments) {
+//     //     console.log(arg);
+//     //     total += arg;
+//     // }
+
+//     // for (let i = 0; i < arguments.length; i += 1) {
+//     //     total += arguments[i];
+//     //        }
+//     return total;
+// }
+
+// getSum(1, 2, 3, 4, 5, 6, 7, 8);
+// getSum(44, 5, 14, 3);
+
+// console.log(getSum(1, 2, 3, 4, 5, 6, 7, 8));
+// console.log(getSum(44, 5, 14, 3));
+
+
+// Task 12
+
+// Напишіть функції для роботи з колекцією навчальних курсів courses:
+// 1 addCourse(name) - додає курс в кінець колекції
+// 2 removeCourse(name) - видаляє курс з колекції
+// 3 updateCourse(oldName, newName) - замінює назву на нову
+
+// const courses = ['HTML', 'CSS', 'JavaScript', 'React', 'Node'];
+
+// function addCourse(name) {
+//     if (!courses.includes(name)) {
+//         courses.push(name);
+//         return 'Курс успішно додан';
+//     }
+//     return 'Ви вже маєте данний курс';
+    
+// }
+// const result = addCourse("Express");
+// console.log(result);
+
+
+// const courses = ['HTML', 'CSS', 'JavaScript', 'React', 'Node'];
+// function removeCourse(name) {
+//     if (courses.includes(name)) {
+//         const idx = courses.indexOf(name);
+//         courses.splice(idx,1);
+//         console.log(idx);
+//         return `Курс ${name} видален`
+//     }
+//     return 'Курс з такою назвою не знайдено'
+// }
+
+// console.log(removeCourse('React'));
+// console.log(courses);
+
+// console.log(removeCourse('Vue'))
+
+// const courses = ['HTML', 'CSS', 'JavaScript', 'React', 'Node', 'Express'];
+// function updateCourse(oldName, newName) { 
+//     if (courses.includes(oldName)) { 
+//         const idx = courses.indexOf(oldName);
+//         //    courses.splice(idx,1,newName)===
+
+//         courses[idx] = newName;
+//     }
+//     return 'Курс з такою назвою не знайдено';
+
+// }
+// // updateCourse('Express')
+// console.log(updateCourse('Express', 'NestJS'));
+// console.log(courses);
+// // console.log(courses); // ['HTML', 'CSS', 'JavaScript', 'PostgreSQL', 'NestJS']
+// console.log(updateCourse('Angular', 'NestJS')); //'Курс з такою назвою не знайдено'
