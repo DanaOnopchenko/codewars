8deff41c0f5334b7ff87aade9c8476899d76efb7
DanaOnopchenko <106885955+DanaOnopchenko@users.noreply.github.com>
2024-01-20 19:43:15 +0200

    update

--- js/tasks.js
+++ js/tasks.js
@@ -382,22 +382,235 @@
 
 
 
-function findLongestWord(string) {
-  // Change code below this line
-  let words = string.split(' ');
-  console.log(words);
-  let longWord = '';
-  for (let word of words) { 
-    console.log(word.length);
-    if (word.length > longWord.length) { 
-      
-      longWord = word;
-    }
-    
-return longWord
-  }
+// function findLongestWord(string) {
+//   // Change code below this line
+//   let words = string.split(' ');
+//   let longWord = '';
+//   for (const word of words) {
+//     if (word.length > longWord.length) {
+//       longWord = word;
+//     }
+//   }
+//       return longWord
+  
+//     // Change code above this line
+  
+// }
+// console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
+// console.log(findLongestWord("Google do a roll"));
+// console.log(findLongestWord("May the force be with you"));
 
+
+// function createArrayOfNumbers(min, max) {
+//   const numbers = [];
+//   // Change code below this line
+// for (let i = min; i <= max; i++) {
+//     numbers.push(i);
+//   }
+//   // Change code above this line
+//   return numbers;
+// }
+
+// console.log(createArrayOfNumbers(1, 3));
+// console.log(createArrayOfNumbers(14, 17));
+// console.log(createArrayOfNumbers(29, 34));
+
+
+// function filterArray(numbers, value) {
+//    // Change code below this line
+//   let numberArr = [];
+//   for (let i = 0; i < numbers.length; i++) {
+//     // const element = numbers[i];
+//     if (numbers[i]> value) {
+//      numberArr.push(numbers[i])
+//     }
+//       }
+//   return numberArr
+
+//   // Change code above this line
+// }
+
+// console.log(filterArray([1, 2, 3, 4, 5], 3));
+// console.log(filterArray([1, 2, 3, 4, 5], 4));
+// console.log(filterArray([1, 2, 3, 4, 5], 5));
+// console.log(filterArray([12, 24, 8, 41, 76], 38));
+// console.log(filterArray([12, 24, 8, 41, 76], 20));
+
+
+// function checkFruit(fruit) {
+//   const fruits = ["apple", "plum", "pear", "orange"];
+
+//   return fruits.includes(fruit); // Change this line
+// }
+// console.log(checkFruit("plum"));
+// console.log(checkFruit("mandarin"));
+// console.log(checkFruit("pear"));
+// console.log(checkFruit("Pear"));
+// console.log(checkFruit("apple"));
+
+
+// function getCommonElements(array1, array2) {
+//   // Change code below this line
+
+//   const numbers = array1;
+//   let uniqueNumbers = [];
+//   for (let number of numbers) {
+//     if (array2.includes(number)) {
+//       uniqueNumbers.push(number)
+//     }
+//   }
+// return uniqueNumbers
+//  // Change code above this line
+// }
+
+// console.log(getCommonElements([1, 2, 3], [2, 4]));
+// console.log(getCommonElements([1, 2, 3], [2, 1, 17, 19]));
+// console.log(getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27]));
+// console.log(getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40]));
+// console.log(getCommonElements([1, 2, 3], [10, 20, 30]));
+
+
+// function calculateTotalPrice(order) {
+//   let total = 0;
+//   // Change code below this line
+
+//   // for (let i = 0; i < order.length; i += 1) {
+//   //   total += order[i];
+//   // }
+
+//   for (let el of order) {
+//     total += el;
+//   }
+//   // Change code above this line
+//   return total;
+// }
+
+// console.log(calculateTotalPrice([12, 85, 37, 4]));
+// console.log(calculateTotalPrice([164, 48, 291]));
+// console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
+
+
+// function filterArray(numbers, value) {
+//   // Change code below this line
+//   const filteredNumbers = [];
+
+  // for (let i = 0; i < numbers.length; i += 1) {
+  //   const number = numbers[i];
+
+  //   if (number > value) {
+  //     filteredNumbers.push(number);
+  //   }
+  // }
+
+//   for (let number of numbers) {
+//     if (number > value) {
+//       filteredNumbers.push(number);
+//     }
+//   }
+
+//   return filteredNumbers;
+//   // Change code above this line
+// }
+// console.log(filterArray([1, 2, 3, 4, 5], 3));
+// console.log(filterArray([1, 2, 3, 4, 5], 4));
+// console.log(filterArray([1, 2, 3, 4, 5], 5));
+// console.log(filterArray([12, 24, 8, 41, 76], 38));
+// console.log(filterArray([12, 24, 8, 41, 76], 20));
+
+
+
+// const a = 3 % 3;
+// const b = 4 % 3;
+// const c = 11 % 4;
+// const d = 12 % 7;
+// const e = 8 % 3;
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(e);
+
+
+
+// function getEvenNumbers(start, end) {
+//    // Change code below this line
+//   const allEventNumber = [];
+//   for (let i = start; i <= end; i++) {
+//     // const element = i;
+//     // console.log(element);
+//     if (i % 2 === 0) {
+//       allEventNumber.push(i)
+//     }
+//       }
+// return allEventNumber
+
+//     // Change code above this line
+// }
+  
+// console.log(getEvenNumbers(2, 5));
+// console.log(getEvenNumbers(3, 11));
+// console.log(getEvenNumbers(6, 12));
+// console.log(getEvenNumbers(8, 8));
+// console.log(getEvenNumbers(7, 7));
+
+
+// const start = 6;
+// const end = 27;
+// let number;
+
+// for (let i = start; i < end; i += 1) {
+//    if (i % 5 === 0 ) {
+//     number = i;
+// break
+//     }
+//   }
+// console.log(number);
+
+
+// function findNumber(start, end, divisor) {
+//   // Change code below this line
+//   let number;
+
+//   for (let i = start; i < end; i += 1) {
+//     if (i % divisor === 0) {
+//       number = i;
+//       return i
+//       }
+//   }
+
+//   // return number;
+//   // Change code above this line
+// }
+
+
+// console.log(findNumber(2, 6, 5));
+// console.log(findNumber(8, 17, 3));
+// console.log(findNumber(6, 9, 4));
+// console.log(findNumber(16, 35, 7));
+
+
+
+function includes(array, value) {
+  // Change code below this line
+
+  // const array = array;
+  for (const el of array) { 
+    if (el === value) { 
+      return true
+    }
+  }
+  return false
   // Change code above this line
 }
 
-console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
+
+
+
+console.log(includes([1, 2, 3, 4, 5], 3));
+console.log(includes([1, 2, 3, 4, 5], 17));
+console.log(includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Jupiter"));
+console.log(includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Uranus"));
+console.log(includes(["apple", "plum", "pear", "orange"], "plum"));
+console.log(includes(["apple", "plum", "pear", "orange"], "kiwi"));
